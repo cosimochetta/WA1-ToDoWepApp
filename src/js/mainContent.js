07d461922e4b45be0e3ebb690ee92dc6d79cecb5
COSIMO CHETTA s271819 <s271819@studenti.polito.it>
2020-05-13 18:54:50 +0200

    fix: change folder

--- src/js/mainContent.js
+++ src/js/mainContent.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import {TaskList} from "./taskList.js"
+import { TaskList } from "./taskList.js"
 
 
 
@@ -14,11 +14,17 @@ class MainContent extends React.Component {
 		};
 	}
 
-	
+	static getDerivedStateFromProps(nextProps, prevState) {
+		return {
+			taskList: [...nextProps.taskList],
+			filter: nextProps.filter
+		};
+	}
+
 
 	render() {
 		return <main class="col-sm-9 col-12 below-nav" >
-			<h1 class="bg-primary text-white d-inline-block p-2 rounded-top">{this.props.filter}</h1>
+			<h1 class="bg-primary text-white d-inline-block p-2 rounded-top">{this.state.filter}</h1>
 			<TaskList taskList={this.state.taskList}></TaskList>
 			<button type="button" class="btn btn-lg btn-primary fixed-right-bottom">&#43;</button>
 		</main>
